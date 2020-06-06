170bb027c76bb0f599f2aa7f67892a36d659958f
elvankarahan <37115884+elvankarahan@users.noreply.github.com>
2020-06-06 22:23:54 +0300

    +INotifyPropertyChanged implemented +Different links added

--- CocktailRecipes/ViewModels/MainViewModel.cs
+++ CocktailRecipes/ViewModels/MainViewModel.cs
@@ -2,35 +2,83 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace CocktailRecipes.ViewModels
 {
-    public class MainViewModel : BaseViewModel
+
+    public class MainViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public IList<Drink> ItemList { get; set; }
+        private IList<Drink> ItemList_;
+
+        public IList<Drink> ItemList
+        {
+            get { return ItemList_; }
+            set
+            {
+                ItemList_ = value;
+                SetProperty(ref ItemList_, value);
+            }
+
+        }
+
+        public ICommand ChangeList { get; }
 
         public string popular = "https://www.thecocktaildb.com/api/json/v2/9973533/popular.php";
         public string randomSelection = "https://www.thecocktaildb.com/api/json/v2/9973533/popular.php";
         public string alcoholicOnly = "http://www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Alcoholic";
         public string nonAlcoholicOnly = "http://www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Non_Alcoholic";
+        public string OptionalAlcohol = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Optional_Alcohol";
 
         public MainViewModel()
         {
             ItemList = new ObservableRangeCollection<Drink>();
-
+            
+            ChangeList = new Command(GetDataForChangeList);
             GetDataAsync(randomSelection);
+        }
+
+        private async void GetDataForChangeList()
+        {
+            string inputUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Non_Alcoholic";
+            HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(inputUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                ItemViewModel rootObject = JsonConvert.DeserializeObject<ItemViewModel>(content);
+                ItemList.Clear();
+                string tempUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=";
+
+                foreach (var item in rootObject.drinks)
+                {
+                    var tempresponse = await httpClient.GetAsync(tempUrl + item.idDrink);
+                    var tempcontent = await tempresponse.Content.ReadAsStringAsync();
+                    ItemViewModel temprootObject = JsonConvert.DeserializeObject<ItemViewModel>(tempcontent);
+
+                    ItemList.Add(temprootObject.drinks[0]);
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Veri çekilirken bir hata oluştu!!");
+            }
         }
 
         private async void GetDataAsync(String restUrl)
         {
             HttpClient httpClient = new HttpClient();
-            //var restUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php";
-            //var restUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/popular.php";
+            //var restUrl1 = "https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php";
+            //var restUrl2 = "https://www.thecocktaildb.com/api/json/v2/9973533/popular.php";
 
             var response = await httpClient.GetAsync(restUrl);
-
+            ItemList.Clear();
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
@@ -40,7 +88,6 @@ namespace CocktailRecipes.ViewModels
                 {
                     ItemList.Add(item);
                 }
-
             }
             else
             {
---
+++ CocktailRecipes/ViewModels/nameThumbid.cs
@@ -0,0 +1,22 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace CocktailRecipes.ViewModels
+{
+
+    public class nameThumbid : ObservableObject
+    {
+        public IList<Drinks> DrinknameThumbiddrinks { get; set; }
+    }
+
+    public class Drinks
+    {
+        public string strDrink { get; set; }
+        public string strDrinkThumb { get; set; }
+        public string idDrink { get; set; }
+    }
+}
